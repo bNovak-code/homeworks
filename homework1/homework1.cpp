d54a6257686cfdcbab1f6833f0a361481ac9e861
y.kovalenko <lisa.kovalenko00@gmail.com>
2024-04-30 18:11:54 +0200

    added homework 1

---
+++ homework1/homework1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*
+1.програма робить перенос на наступну строку, потім виводить суму x i y і через пробіл виводить результат множення.
+2.так
+3.помилки:
+ - 'include' does not name a type
+ - include is not a template
+ - indentifier "iostream" is undefined
+ - expected an identifier
+ - 'cout' was not declared in this scope
+ - identifier "cout" is undefined
+помилки виникли тому що немає # перед include
+4.виводить "hello world"*/
+
+int main()
+{
+	cout << "Hello World\n";
+	return 0;
+}
